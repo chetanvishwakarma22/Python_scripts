0e4a0d5866449a3556d8fd8d703e080d7b4b82f1
CHETAN <chetan22vishwakarma@gmail.com>
2025-07-18 20:33:58 +0530

    Update LIC_NOT_path_cont_add_new_file22.py

--- LIC_NOT_path_cont_add_new_file22.py
+++ LIC_NOT_path_cont_add_new_file22.py
@@ -18,3 +18,4 @@ for path, dir, files in os.walk("/home/ubuntu/python/hadoop-1"):
             with open("LIC_NOT.txt", "a") as fh:
                 fh.write("\nPath of the file is : "+os.path.join(path,file))
                 fh.write("\nContent of the file is : "+var)
+            
